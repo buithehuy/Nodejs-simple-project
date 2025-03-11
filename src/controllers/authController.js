4347889121235091f79ba25fcaf3662caf860f1e
HUY Bui The (VTI.D8) <huy.buithe@vti.vn>
2025-03-11 09:51:15 +0700

    add authController

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -24,8 +24,31 @@ const register = (req, res) => {
     })
 };
 
+const login = (req, res) => {
+    const {username, password} = req.body;
+    if(!username || !password) {
+        return res.status(400).json({success: false, message: 'fill full form'});
+    }
+
+    connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, result) => {
+        if (err) {
+            return res.status(500).json({ success: false, message: 'database err' });
+        }
+
+        if (result.length === 0) {
+            return res.status(401).json({ success: false, message: 'username or password is incorrect' });
+        }
+
+        req.session.loggedin = true;
+        req.session.username = username;
+        req.session.userId = result[0].id;
+        
+        res.redirect('/profile');
+        
+    });
+};
 
 
 module.exports = {
-    register
+    register, login
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 
 const express = require('express');
-const bcrypt = require('bcrypt');
+const { register, login } = require('./controllers/authController');
 
 const connection = require('./config/db');
 
@@ -70,53 +70,9 @@ app.get('/login', function (req, res) {
  }
 )
 
-app.post('/register', (req, res) => {
-    const {username, password} = req.body;
-    if(!username||!password) {
-        return res.status(400).json({ success: false, message: 'fill full form' });
-    }
+app.post('/register', register);
 
-    connection.query('SELECT * FROM users WHERE username = ?', [username], (err, result) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'database err' });
-        }
-
-        if (result.length > 0) {
-            return res.status(409).json({ success: false, message: 'exist user' });
-        }
-
-        connection.query('INSERT INTO users (username, password) VALUE (?, ?)', [username, password], (err, result) => {
-            if (err) {
-                return res.status(500).json({ success: false, message: 'database err' });
-            }
-            return res.status(201).json({ success: true, message: 'sucess' });
-        })
-    })
-});
-
-app.post('/login', (req, res) => {
-    const {username, password} = req.body;
-    if(!username || !password) {
-        return res.status(400).json({success: false, message: 'fill full form'});
-    }
-
-    connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, result) => {
-        if (err) {
-            return res.status(500).json({ success: false, message: 'database err' });
-        }
-
-        if (result.length === 0) {
-            return res.status(401).json({ success: false, message: 'username or password is incorrect' });
-        }
-
-        req.session.loggedin = true;
-        req.session.username = username;
-        req.session.userId = result[0].id;
-        
-        res.redirect('/profile');
-        
-    });
-});
+app.post('/login', login);
 
 app.get('/profile', requireLogin, (req, res) => {
     connection.query(
